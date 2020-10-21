faf37df2d83e9556cace3284918bad7622b51328
daniel140799 <danny6365991@hotmail.com>
2020-10-21 18:18:09 -0500

    objetivo1.py uno agregue ni mierda xcdfgdgfs gjfhgfj  enter the commit message for your changes. Lines starting objetivo1.py

---
+++ objetivo1.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+x = 0.4
+y = 1.3
+z = x+y
+print(z)
+
+h = np.cos(x)
+print(h)
+
+g = np.exp(2j*3.141592*x)
+print(g)
+
+
+x=np.array([0.1, 0.2, 0.4])
+y=np.array([2.0, 3.0, 5.1])
+
+z = x+y
+print(z)
+
+h = np.cos(x)
+print(h)
+
+g = np.exp(2j*3.141592*x)
+print(g)
+
+
+print("este es un cambio hecho para probar que se cambio en git")
